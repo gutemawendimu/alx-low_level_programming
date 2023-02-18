f21c8f2cfd5a559d842fdf73580630ddafad5a9e
gutemawendimu <wandimugt@gmail.com>
2023-02-18 03:49:25 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,9 +12,5 @@ long long int d;
 float f;
 
 printf("Size of char:%lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of char:%lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of char:%lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of char:%lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of char:%lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
