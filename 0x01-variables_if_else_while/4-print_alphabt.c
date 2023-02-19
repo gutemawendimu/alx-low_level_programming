7d12c1780e58bd0f405a4dfc8cd99412f5c9c2dc
gutemawendimu <wandimugt@gmail.com>
2023-02-19 06:09:01 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Return: Always 0 (Success)
+  */
+int main(void)
+{
+char x;
+x = 'a';
+while (x <= 'z')
+{
+if ((x != 'q' && x != 'e') && x <= 'z')
+putchar(x);
+x++;
+}
+putchar('\n');
+return (0);
+}
