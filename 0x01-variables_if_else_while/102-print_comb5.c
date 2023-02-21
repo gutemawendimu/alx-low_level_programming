9ee510c670b8533872be10102136ad46d447fc2a
gutemawendimu <wandimugt@gmail.com>
2023-02-21 06:25:17 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,15 +8,15 @@
 int main(void)
 {
 int a, b;
-for (a = 0; a <= 98; a++)
+for (a = 0; a <= 100; a++)
 {
-for (b = a + 1; b <= 98; b++)
+for (b = a + 1; b <= 100; b++)
 {
-putchar((a / 10) + '0');
-putchar((a % 10) + '0');
+putchar((a / 10) + 48);
+putchar((a % 10) + 48);
 putchar(' ');
-putchar((b / 10) + '0');
-putchar((b % 10) + '0');
+putchar((b / 10) + 48);
+putchar((b % 10) + 48);
 if (a == 98 && b == 99)
 continue;
 putchar(',');
