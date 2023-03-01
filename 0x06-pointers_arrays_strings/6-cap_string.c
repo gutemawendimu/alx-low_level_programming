1d5b9b9c82e9af7e14f1c8299084d1f5f6ddd405
gutemawendimu <wandimugt@gmail.com>
2023-03-01 10:53:50 -0800

    6-cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,34 @@
 #include "main.h"
-
 /**
- * cap_string - Write a function that capitalizes all words of a string.
+ * cap_string - capitalizes all words of a string
+ * @str: the string to change the first letter of a word in uppercase
  *
- * @entry: This is the input string
- *
- * Return: String capitalized
+ * Return: capitalizes letters
  */
-char *cap_string(char *entry)
+char *cap_string(char *str)
 {
-	int conversion, index, count;
+  int i;
+  int j;
+  char c[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
 
-	char chars[] = {' ', ',', ';', '.', '!',
-			 '?', '"', '(', ')', '{', '}',  '\t', '\n', '\0'};
-	conversion = 32;
+  i = 0;
 
-	for (index = 0; entry[index] != '\0'; index++)
+  while (str[i] != '\0')
+    {
+      if (i == 0 && str[i] >= 97 && str[i] <= 122)
 	{
-		if (entry[index] >= 'index' && entry[index] <= 'z')
-		{
-			entry[index] =  entry[index] - conversion;
-		}
-		conversion = 0;
-		for (count = 0; chars[count] != '\0'; count++)
-		{
-			if (chars[count] == entry[index])
-			{
-				conversion = 32;
-				break;
-			}
-		}
+	  str[i] = str[i] - 32;
 	}
-	return (entry);
+      j = 0;
+      while (c[j] != '\0')
+	{
+	  if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
+	    {
+	      str[i + 1] = str[i + 1] - 32;
+	    }
+	  j++;
+	}
+      i++;
+    }
+  return (str);
 }
