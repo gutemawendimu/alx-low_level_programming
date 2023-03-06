793abad7d58dba2355210ed09cef92f0fa53b67d
gutemawendimu <wandimugt@gmail.com>
2023-03-06 03:46:56 -0800

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,20 +9,19 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
-	while (*haystack != '\0')
-	{
-		char *duplicate = haystack;
-
-		while (*haystack == *needle && *haystack != '\0' && *needle != '\0')
-		{
-			needle++;
-			haystack++;
-		}
-		if (!*needle != '\0')
-		{
-			return (duplicate);
-		}
-		haystack++;
-	}
-	return (0);
+while (*haystack != '\0')
+{
+char *duplicate = haystack;
+while (*haystack == *needle && *haystack != '\0' && *needle != '\0')
+{
+needle++;
+haystack++;
+}
+if (!*needle != '\0')
+{
+return (duplicate);
+}
+haystack++;
+}
+return (0);
 }
