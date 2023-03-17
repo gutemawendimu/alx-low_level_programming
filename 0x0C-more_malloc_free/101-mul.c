a480e5dbb5d630d446a6414bda5a6c9edd2982f3
gutemawendimu <wandimugt@gmail.com>
2023-03-17 01:25:43 -0700

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	{
+		printf("Hel, World, World, Hel.\n");
+
+		return (0);
+	}
