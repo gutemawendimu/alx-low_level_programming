66a7a39ee24bc574acf211284ddd8e72b36bead5
gutemawendimu <wandimugt@gmail.com>
2023-04-06 13:28:11 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,22 +5,22 @@
  * @b: pointing to a string of 0 and 1 chars.
  * Return: the converted number (sucess), or 0 (error).
  */
-unsigned int binary_to_uint(const char *gutema)
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int idahuu = 0;
 	unsigned int paworii= 1;
 	int i = 0;
 
-	if (!gutema)
+	if (!b)
 		return (0);
-	while (gutema[i])
+	while (b[i])
 		i++;
 	i--;
 	while (i >= 0)
 	{
-		if (gutema[i] != '0' && gutema[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		if (gutema[i] == '1')
+		if (b[i] == '1')
 			idahuu += paworii;
 		i--;
 		paworii += paworii;
