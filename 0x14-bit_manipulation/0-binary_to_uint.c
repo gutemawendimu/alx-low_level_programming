7c65a2a8e0a19c2d15a5eede2172b844dccc9f4a
gutemawendimu <wandimugt@gmail.com>
2023-05-04 07:21:55 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -20,23 +20,23 @@ return (y);
  *
  * Return:unsigned int
  */
-unsigned int binary_to_uint(const char *z)
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int k = 1;
 	unsigned int y = 0;
 	int c;
 	unsigned int len;
 
-	if (z == NULL)
+	if (b == NULL)
 		return (0);
 
-	len = _strlen(z);
+	len = _strlen(b);
 
 	for (c = len - 1; c >= 0; c--)
 	{
-	if (z[c] != '0' && z[c] != '1')
+	if (b[c] != '0' && b[c] != '1')
 		return (0);
-	if (z[c] == '1')
+	if (b[c] == '1')
 	{
 		y += k;
 	}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,7 +2,7 @@
 #define _MAIN_H_
 #include <stddef.h>
 
-unsigned int binary_to_uint(const char *z);
+unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
